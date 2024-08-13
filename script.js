e748a25c7b9935dce004acb5157977b61cb2b6bf
eliucinho <36527123+eliucinho@users.noreply.github.com>
2024-08-13 21:42:22 +0200

    se cambian por objetos los ejercicios

--- script.js
+++ script.js
@@ -34,15 +34,30 @@ fetch('ejercicios.json')
     .then(response => response.json())
     .then(data => {
         const ejercicioTable = document.getElementById('ejercicioTable');
+        
         data.rutina.forEach(dia => {
             const row = document.createElement('tr');
+            let ejerciciosHTML = '';
+
+            dia.ejercicios.forEach(ejercicio => {
+                ejerciciosHTML += `
+                    <strong>Ejercicio:</strong> ${ejercicio.nombre}<br>
+                    <strong>Peso:</strong> ${ejercicio.peso}<br>
+                    <strong>Repeticiones:</strong> ${ejercicio.numeroRepeticion}<br>
+                    <strong>Series:</strong> ${ejercicio.series}<br>
+                    <strong>Objetivo:</strong> ${ejercicio.objetivo}<br>
+                    <strong>Nivel:</strong> ${ejercicio.nivel}<br><br>
+                `;
+            });
+
             row.innerHTML = `
                 <td>${dia.dia}</td>
                 <td>${dia.calentamiento}</td>
-                <td>${dia.ejercicios.join('<br>')}</td>
+                <td>${ejerciciosHTML}</td>
                 <td>${dia.cardio}</td>
                 <td>${dia.estiramientos}</td>
             `;
             ejercicioTable.appendChild(row);
         });
     });
+})
