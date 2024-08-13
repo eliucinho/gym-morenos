79b2ba245591073651c705a9430e26ad16e316d1
eliucinho <36527123+eliucinho@users.noreply.github.com>
2024-08-13 21:53:19 +0200

    nueva forma de visualizar el contenido por dia

--- script.js
+++ script.js
@@ -1,63 +1,87 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Cambiar pestañas
-    const tabLinks = document.querySelectorAll(".tab-link");
-    tabLinks.forEach(link => {
-        link.addEventListener("click", function () {
-            tabLinks.forEach(btn => btn.classList.remove("active"));
-            this.classList.add("active");
+    // Detectar el día actual
+    const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const today = new Date().getDay();
+    const todayName = days[today];
 
-            const tabContent = document.querySelectorAll(".tab-content");
-            tabContent.forEach(content => content.classList.remove("active"));
-            document.getElementById(this.dataset.tab).classList.add("active");
-        });
+    // Cargar el JSON de ejercicios
+    fetch('ejercicios.json')
+        .then(response => response.json())
+        .then(data => {
+            const exerciseTabs = document.getElementById('exerciseTabs');
+            const exerciseContent = document.getElementById('exerciseContent');
 
-                     });
+            data.rutina.forEach((dia, index) => {
+                const isActive = dia.dia === todayName ? 'active' : '';
 
-// Cargar el JSON de comida
-fetch('comida.json')
-    .then(response => response.json())
-    .then(data => {
-        const comidaTable = document.getElementById('comidaTable');
-        data.comidas.forEach(comida => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${comida.comida}</td>
-                <td>${comida.semana}</td>
-                <td>${comida.finDeSemana}</td>
-            `;
-            comidaTable.appendChild(row);
-        });
-    });
-
-// Cargar el JSON de ejercicios
-fetch('ejercicios.json')
-    .then(response => response.json())
-    .then(data => {
-        const ejercicioTable = document.getElementById('ejercicioTable');
-        
-        data.rutina.forEach(dia => {
-            const row = document.createElement('tr');
-            let ejerciciosHTML = '';
-
-            dia.ejercicios.forEach(ejercicio => {
-                ejerciciosHTML += `
-                    <strong>Ejercicio:</strong> ${ejercicio.nombre}<br>
-                    <strong>Peso:</strong> ${ejercicio.peso}<br>
-                    <strong>Repeticiones:</strong> ${ejercicio.numeroRepeticion}<br>
-                    <strong>Series:</strong> ${ejercicio.series}<br>
-                    <strong>Objetivo:</strong> ${ejercicio.objetivo}<br>
-                    <strong>Nivel:</strong> ${ejercicio.nivel}<br><br>
+                // Crear tabs para cada día
+                const tabButton = document.createElement('li');
+                tabButton.className = `nav-item`;
+                tabButton.innerHTML = `
+                    <a class="nav-link ${isActive}" id="exercise-tab-${index}" data-toggle="tab" href="#exercise-${index}" role="tab" aria-controls="exercise-${index}" aria-selected="${isActive === 'active'}">
+                        ${dia.dia}
+                    </a>
                 `;
-            });
+                exerciseTabs.appendChild(tabButton);
 
-            row.innerHTML = `
-                <td>${dia.dia}</td>
-                <td>${dia.calentamiento}</td>
-                <td>${ejerciciosHTML}</td>
-                <td>${dia.cardio}</td>
-                <td>${dia.estiramientos}</td>
-            `;
-            ejercicioTable.appendChild(row);
+                // Crear contenido para cada día
+                const tabPane = document.createElement('div');
+                tabPane.className = `tab-pane fade show ${isActive}`;
+                tabPane.id = `exercise-${index}`;
+                tabPane.setAttribute('role', 'tabpanel');
+                tabPane.innerHTML = `
+                    <div class="exercise-details"><strong>Calentamiento:</strong> ${dia.calentamiento}</div>
+                    <div class="exercise-details">
+                        <strong>Ejercicios:</strong>
+                        <ul>
+                            ${dia.ejercicios.map(ejercicio => `
+                                <li>
+                                    <strong>${ejercicio.nombre}:</strong> ${ejercicio.peso}, ${ejercicio.numeroRepeticion} repeticiones, ${ejercicio.series} series, 
+                                    Objetivo: ${ejercicio.objetivo}, Nivel: ${ejercicio.nivel}
+                                </li>`).join('')}
+                        </ul>
+                    </div>
+                    <div class="exercise-details"><strong>Cardio:</strong> ${dia.cardio}</div>
+                    <div class="exercise-details"><strong>Estiramientos:</strong> ${dia.estiramientos}</div>
+                `;
+                exerciseContent.appendChild(tabPane);
+            });
         });
-    });
-})
+
+    // Cargar el JSON de comidas
+    fetch('comida.json')
+        .then(response => response.json())
+        .then(data => {
+            const foodTabs = document.getElementById('foodTabs');
+            const foodContent = document.getElementById('foodContent');
+
+            data.comidas.forEach((comida, index) => {
+                const isActive = comida.dia === todayName ? 'active' : '';
+
+                // Crear tabs para cada día
+                const tabButton = document.createElement('li');
+                tabButton.className = `nav-item`;
+                tabButton.innerHTML = `
+                    <a class="nav-link ${isActive}" id="food-tab-${index}" data-toggle="tab" href="#food-${index}" role="tab" aria-controls="food-${index}" aria-selected="${isActive === 'active'}">
+                        ${comida.dia}
+                    </a>
+                `;
+                foodTabs.appendChild(tabButton);
+
+                // Crear contenido para cada día
+                const tabPane = document.createElement('div');
+                tabPane.className = `tab-pane fade show ${isActive}`;
+                tabPane.id = `food-${index}`;
+                tabPane.setAttribute('role', 'tabpanel');
+                tabPane.innerHTML = `
+                    <div class="food-details"><strong>Desayuno:</strong> ${comida.desayuno}</div>
+                    <div class="food-details"><strong>Media Mañana:</strong> ${comida.mediaManana}</div>
+                    <div class="food-details"><strong>Almuerzo:</strong> ${comida.almuerzo}</div>
+                    <div class="food-details"><strong>Merienda:</strong> ${comida.merienda}</div>
+                    <div class="food-details"><strong>Cena:</strong> ${comida.cena}</div>
+                    <div class="food-details"><strong>Antes de Dormir:</strong> ${comida.antesDeDormir}</div>
+                `;
+                foodContent.appendChild(tabPane);
+            });
+        });
+});
